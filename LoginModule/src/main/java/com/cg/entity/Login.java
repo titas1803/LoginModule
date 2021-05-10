e1be9934d4a569a8cc67cbfe1bc4765a34fa5aa0
titas1803 <titas.sarkar420@hotmail.com>
2021-05-10 14:00:12 +0530

    Login entity created

--- LoginModule/src/main/java/com/cg/entity/Login.java
+++ LoginModule/src/main/java/com/cg/entity/Login.java
@@ -1,5 +1,62 @@
 package com.cg.entity;
 
-public class Login {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "login_module_login")
+public class Login {
+	
+//	@Id
+//	@Column(name = "login_id")
+//	private Integer userId;
+	
+	@Column(name="login_password")
+	private String password;
+	
+	@Id
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "login_Id")
+	private User user;
+
+	public Login() {
+		
+	}
+	
+	public Login(String password, User user) {
+		this.password = password;
+		this.user = user;
+	}
+
+//	public Integer getUserId() {
+//		return userId;
+//	}
+//
+//	public void setUserId(Integer userId) {
+//		this.userId = userId;
+//	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
 }
