f5d25e6dfd146bf77633a83683ba85307652766f
titas1803 <titas.sarkar420@hotmail.com>
2021-05-16 10:55:45 +0530

    Update Titas 7

---
+++ LoginModule/src/main/java/com/cg/login/service/LoginServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cg.login.service;
+
+public class LoginServiceImpl implements ILoginService {
+
+	@Override
+	public Integer createLoginAccount(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
