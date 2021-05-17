15c308110916bcb6ec20e24178e447d1074344c2
lenovo <lenovo@DESKTOP-BHD7EAS>
2021-05-17 09:43:34 +0530

    Soumendu

--- LoginModule/src/main/java/com/cg/login/service/IUserService.java
+++ LoginModule/src/main/java/com/cg/login/service/IUserService.java
@@ -3,13 +3,14 @@ package com.cg.login.service;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.cg.login.dto.UserDto;
 import com.cg.login.entity.User;
 import com.cg.login.exceptions.UserNotFoundException;
 
 public interface IUserService {
 
-	public Integer createUser(String userName, String contactNo, String emailId, LocalDate userDob
-			, String userAddress, String location);
+	public Integer createUser(UserDto userdto);
 	public List<User> viewAllUser() throws UserNotFoundException;
 	public User viewUserById(int userId) throws UserNotFoundException;
+	
 }
